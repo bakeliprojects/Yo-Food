8dc1b408d41b43d85709f14ef19dd72095b72d63
Yamadou Traore <traorey@seattleu.edu>
2017-08-02 11:34:53 -0700

    IT WORKSSS

--- FetchData.swift
+++ FetchData.swift
@@ -16,13 +16,14 @@ import RealmSwift
 // This GET request can only create new or update existing items in the Realm database
 
 class FetchData {
+    
     static func get <T: Object> (type: T.Type, success:@escaping () -> Void, fail:@escaping (_ error:NSError)->Void)->Void where T:Mappable, T:Meta {
         
         Alamofire.request(type.url(), method: .get)
             .responseArray { (response: DataResponse<[T]>) in
                 switch response.result {
                 case .success(let items):
-                    print("Waganor")
+                    print(items)
                     autoreleasepool {
                         do {
                             let realm = try Realm()
@@ -37,43 +38,8 @@ class FetchData {
                     }
                     success()
                 case .failure(let error):
-                    print("Yamadooor")
                     fail(error as NSError)
                 }
         }
     }
 }
-
-
-/*
- guard response.result.error == nil else {
- // got an error in getting the data, need to handle it
- print("error calling GET on /todos/1")
- print(response.result.error!)
- return
- }
- 
- // make sure we got some JSON since that's what we expect
- guard let json = response.result.value as? [String: Any] else {
- print("didn't get todo object as JSON from API")
- print("Error: \(String(describing: response.result.error))")
- return
- }
- 
- 
- guard let meals = json["data"] as? [[String: Any]] else {
- print("could not convert data to array")
- return
- }
- 
- 
- try! self.realm.write {
- 
- for meal in meals {
- var newMeal = MenuModel()
- newMeal.meal = meal["name"] as! String
- newMeal.id = meal["id"] as! Int
- self.realm.add(newMeal, update: true)
- }
- 
- }*/
